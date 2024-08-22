45f95861fee8b93f5604e1db6981ead044e9ed56
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-08-22 11:37:09 -0700

    Update config.py

--- config.py
+++ config.py
@@ -30,6 +30,8 @@ PORT = os.environ.get("PORT", "8080")
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://ultroidxTeam:ultroidxTeam@cluster0.gabxs6m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DATABASE_NAME", "ultroidxTeam")
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://ultroidxTeam:ultroidxTeam@cluster0.gabxs6m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001982072622"))
