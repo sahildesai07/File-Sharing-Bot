b8efb672a80dcac6c7bc2456400495bd77fcfd3b
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-11-21 14:58:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,10 @@ import sys
 from datetime import datetime
 
 from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT
+pyrogram.utils.MIN_CHAT_ID = -999999999999
+pyrogram.utils.MIN_CHANNEL_ID = -100999999999999
 
+name ="""By @ultroid_official From Telegram"""
 class Bot(Client):
     def __init__(self):
         super().__init__(
